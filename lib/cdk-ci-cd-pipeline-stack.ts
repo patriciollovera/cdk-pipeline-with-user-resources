e8dc2241c29faeb70f262a7caa742da4388a263a
example-user-at-206251961235 <patricio.llovera.ext@boehringer-ingelheim.com>
2023-09-22 14:52:25 -0300

    Adding Pipelines construct

--- lib/cdk-ci-cd-pipeline-stack.ts
+++ lib/cdk-ci-cd-pipeline-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
+
 // import {CdkPipelineStage} from './pipeline-stage';
 
 export class CdkCiCdPipelineStack extends cdk.Stack {
@@ -10,23 +10,7 @@ export class CdkCiCdPipelineStack extends cdk.Stack {
 
     
 
-    // // The basic pipeline declaration. This sets the initial structure
-    // // of our pipeline
-    // const pipeline = new CodePipeline(this, 'Pipeline', {
-    //   pipelineName: 'TrainingPipeline',
-    //   synth: new CodeBuildStep('SynthStep', {
-    //           input: CodePipelineSource.codeCommit(repo, 'master'),
-    //           installCommands: [
-    //               'npm install -g aws-cdk'
-    //           ],
-    //           commands: [
-    //               'npm ci',
-    //               'npm run build',
-    //               'npx cdk synth'
-    //           ]
-    //       }
-    //   )
-    // });
+    
 
     // const deploy = new CdkPipelineStage(this, 'Deploy');
     // const deployStage = pipeline.addStage(deploy);
--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { userstack } from './stacks/user.resources';
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 
 export class CdkCiCdPipelineStack extends cdk.Stack {
 
@@ -16,8 +17,26 @@ export class CdkCiCdPipelineStack extends cdk.Stack {
         const repo = new codecommit.Repository(this, 'BasicStack', {
         repositoryName: "BasicStack"
         });
-        
+
         new cdk.CfnOutput(this, 'repoHttpUrl', {value: repo.repositoryCloneUrlHttp});
+
+        // // The basic pipeline declaration. This sets the initial structure
+        // of our pipeline
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+          pipelineName: 'TrainingPipeline',
+          synth: new CodeBuildStep('SynthStep', {
+                  input: CodePipelineSource.codeCommit(repo, 'master'),
+                  installCommands: [
+                      'npm install -g aws-cdk'
+                  ],
+                  commands: [
+                      'npm ci',
+                      'npm run build',
+                      'npx cdk synth'
+                  ]
+              }
+          )
+        });
 
 
     }
